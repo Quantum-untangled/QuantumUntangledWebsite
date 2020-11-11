89c71802e5f2184656203faab1eb418dee55587d
Emilio <epelaaez@gmail.com>
2020-11-11 07:51:27 +0100

    Added templates to forum app

--- website/QuantumUntangledWebsite/settings.py
+++ website/QuantumUntangledWebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import psycopg2.extensions # will be used in the future to change database from sqlite to PostgreSQL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'forum.apps.ForumConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- website/forum/views.py
+++ website/forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Quantum Untangled")
+    return render(request, 'forum/index.html')
